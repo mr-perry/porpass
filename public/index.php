de431dba6760c798448f5a0a79b9c896cc7ee978
Matthew R Perry <matt@mr-perry.com>
2026-03-17 10:24:08 -0600

    Hello World PHP Test

--- public/index.php
+++ public/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
+<?php
+    echo "<title>Hello, World!</title>";
+?>
 </head>
 <body>
-    <h1>Hello World!</h1>
+<?php
+    echo "<h1>Hello World, PHP Style!</h1>";
+?>
 </body>
 </html>
